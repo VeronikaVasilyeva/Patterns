fb1cd7411e5fb1ae1cbad74566560e5be71693ad
VV <VV@vv>
2016-11-23 23:26:11 +0500

    problem fixed

--- Adapter/src/com/company/FirstOrmAdapter.java
+++ Adapter/src/com/company/FirstOrmAdapter.java
@@ -10,49 +10,50 @@ import Homework.IFirstOrm;
  */
 public class FirstOrmAdapter implements ITarget{
 
-    private final IFirstOrm<IDbEntity> orm;
+    private final IFirstOrm<DbUserEntity> orm1;
+    private final IFirstOrm<DbUserInfoEntity> orm2;
 
-    public FirstOrmAdapter(IFirstOrm orm) {
-        this.orm = orm;
+    public FirstOrmAdapter(IFirstOrm<DbUserEntity> orm1, IFirstOrm<DbUserInfoEntity> orm2) {
+        this.orm1 = orm1; this.orm2= orm2;
     }
 
     @Override
     public void createUserEntity(DbUserEntity entity) {
-        orm.create(entity);
+        orm1.create(entity);
     }
 
     @Override
     public void createUserInfoEntity(DbUserInfoEntity entity) {
-        orm.create(entity);
+        orm2.create(entity);
     }
 
     @Override
     public DbUserEntity readUserEntity(int id) {
-        return (DbUserEntity) orm.read(id);
+        return (DbUserEntity) orm1.read(id);
     }
 
     @Override
     public DbUserInfoEntity readUserInfoEntity(int id) {
-        return (DbUserInfoEntity) orm.read(id);
+        return (DbUserInfoEntity) orm2.read(id);
     }
 
     @Override
     public void updateUserEntity(DbUserEntity entity) {
-        orm.update(entity);
+        orm1.update(entity);
     }
 
     @Override
     public void updateUserInfoEntity(DbUserInfoEntity entity) {
-        orm.update(entity);
+        orm2.update(entity);
     }
 
     @Override
     public void deleteUserEntity(DbUserEntity entity) {
-        orm.delete(entity);
+        orm1.delete(entity);
     }
 
     @Override
     public void deleteUserInfoEntity(DbUserInfoEntity entity) {
-        orm.delete(entity);
+        orm2.delete(entity);
     }
 }
