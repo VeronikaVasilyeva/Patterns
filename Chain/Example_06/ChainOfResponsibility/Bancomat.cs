ee4441df2f22a21364ca1a4add3c948bfcb0a1b9
VV <VV@vv>
2016-12-07 00:14:40 +0500

    add chain and state patterns

---
+++ Chain/Example_06/ChainOfResponsibility/Bancomat.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Example_06.ChainOfResponsibility
+{
+    public enum CurrencyType
+    {
+        Eur,
+        Dollar,
+        Ruble
+    }
+
+    public class Banknote
+    {
+        CurrencyType Currency { get; }
+        uint Value { get; }
+
+        public Banknote(CurrencyType c, uint v)
+        {
+            Currency = c;
+            Value = v;
+        }
+    }
+
+    public class Bancomat
+    {
+        private readonly BanknoteHandler _handler;
+
+        public Bancomat()
+        {
+            _handler = new TenRubleHandler(null);
+            _handler = new TenDollarHandler(_handler);
+            _handler = new FiftyDollarHandler(_handler);
+            _handler = new HundredDollarHandler(_handler);
+        }
+
+        public bool Validate(string banknote)
+        {
+            return _handler.Validate(banknote);
+        }
+
+        public Banknote[] CashOut(CurrencyType c, uint v)
+        {
+            var result = _handler.CashOut(c, ref v);
+
+            if (v > 0)
+            {
+                throw new Exception("Введена невалидная сумма");
+            }
+
+            return result;
+        }
+    }
+
+    public abstract class BanknoteHandler
+    {
+        public virtual Banknote[] CashOut(CurrencyType c, ref uint v)
+        {
+            if (_nextHandler != null)
+            {
+                return _nextHandler.CashOut(c, ref v);
+            }
+            return null;
+        }
+
+        private readonly BanknoteHandler _nextHandler;
+
+        protected BanknoteHandler(BanknoteHandler nextHandler)
+        {
+            _nextHandler = nextHandler;
+        }
+
+        public virtual bool Validate(string banknote)
+        {
+            return _nextHandler != null && _nextHandler.Validate(banknote);
+        }
+    }
+
+    public class TenRubleHandler : BanknoteHandler
+    {
+        public override bool Validate(string banknote)
+        {
+            if (banknote.Equals("10 Рублей"))
+            {
+                return true;
+            }
+            return base.Validate(banknote);
+        }
+
+        public TenRubleHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        public override Banknote[] CashOut(CurrencyType c, ref uint v)
+        {
+            if (c == CurrencyType.Ruble)
+            {
+                if (v / 10 > 0)
+                {
+                    var count = v / 10;
+                    var list = new Banknote[count];
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        var bancnote = new Banknote(c, 10);
+
+                        list[i] = bancnote;
+                    }
+                    v -= count * 10;
+                    return list.Concat(base.CashOut(c, ref v)).ToArray();
+                }
+
+                return base.CashOut(c, ref v);
+            }
+            return base.CashOut(c, ref v);
+        }
+    }
+
+    public abstract class DollarHandlerBase : BanknoteHandler
+    {
+        public override bool Validate(string banknote)
+        {
+            if (banknote.Equals($"{Value}$"))
+            {
+                return true;
+            }
+            return base.Validate(banknote);
+        }
+
+        protected abstract uint Value { get; }
+
+        protected DollarHandlerBase(BanknoteHandler nextHandler) : base(nextHandler)
+        { }
+
+        public override Banknote[] CashOut(CurrencyType c, ref uint v)
+        {
+            if (c == CurrencyType.Dollar)
+            {
+                if (v / Value > 0)
+                {
+                    var count = v / Value;
+                    var list = new Banknote[count];
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        var bancnote = new Banknote(c, Value);
+
+                        list[i] = bancnote;
+                    }
+                    v -= count * Value;
+                    return list.Concat(base.CashOut(c, ref v)).ToArray();
+                }
+
+                return base.CashOut(c, ref v);
+            }
+            return base.CashOut(c, ref v);
+        }
+    }
+
+    public class HundredDollarHandler : DollarHandlerBase
+    {
+        protected override uint Value => 100;
+
+        public HundredDollarHandler(BanknoteHandler nextHandler) : base(nextHandler)
+        { }
+
+    }
+
+    public class FiftyDollarHandler : DollarHandlerBase
+    {
+        protected override uint Value => 50;
+
+        public FiftyDollarHandler(BanknoteHandler nextHandler) : base(nextHandler)
+        { }
+    }
+
+    public class TenDollarHandler : DollarHandlerBase
+    {
+        protected override uint Value => 10;
+
+        public TenDollarHandler(BanknoteHandler nextHandler) : base(nextHandler)
+        { }
+    }
+}
---
+++ State/Example_07/CopyMachine/CopyMachine.cs
@@ -0,0 +1,211 @@
+﻿using Example_07.CopyMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_07.CopyMachine
+{
+    enum DeviceType
+    {
+        WIFI,
+        FlashCard
+    }
+
+    class Document
+    {
+        public string Data { get; set; }
+    }
+
+    class CopyMachine
+    {
+        private State state;
+
+        CopyMachine()
+        {
+            state = new EmptyState();
+        }
+
+        public void InsertMoney(uint money)
+        {
+            state = state.InsertMoney(money);
+        }
+
+        public void ChooseDevice(DeviceType dev)
+        {
+            state = state.ChooseDevice(dev);
+        }
+
+        public void SetDocument(Document doc)
+        {
+            state = state.SetDocument(doc);
+        }
+
+        public string PrintDocument()
+        {
+            string result;
+
+            state = state.Print(out result);
+            return result;
+        }
+
+        public uint CheckOut()
+        {
+            uint money;
+            state = state.CheckOut(out money);
+            return money;
+        }
+    }
+
+    abstract class State
+    {
+        internal abstract State CheckOut(out uint money);
+        internal abstract State ChooseDevice(DeviceType dev);
+        internal abstract State InsertMoney(uint money);
+        internal abstract State Print(out string result);
+        internal abstract State SetDocument(Document doc);
+    }
+
+    class EmptyState : State
+    {
+        internal override State CheckOut(out uint money)
+        {
+            throw new Exception();
+        }
+
+        internal override State ChooseDevice(DeviceType dev)
+        {
+            throw new Exception();
+        }
+
+        internal override State InsertMoney(uint money)
+        {
+            return new ChooseDeviceState(money);
+        }
+
+        internal override State Print(out string result)
+        {
+            throw new Exception();
+        }
+
+        internal override State SetDocument(Document doc)
+        {
+            throw new Exception();
+        }
+    }
+
+    class ChooseDeviceState : State
+    {
+        private uint money;
+
+        public ChooseDeviceState(uint money)
+        {
+            this.money = money;
+        }
+
+        internal override State CheckOut(out uint money)
+        {
+            throw new Exception();
+        }
+
+        internal override State ChooseDevice(DeviceType dev)
+        {
+            return new SetDocumentState(money, dev);
+        }
+
+        internal override State InsertMoney(uint money)
+        {
+            throw new Exception();
+        }
+
+        internal override State Print(out string result)
+        {
+            throw new Exception();
+        }
+
+        internal override State SetDocument(Document doc)
+        {
+            throw new Exception();
+        }
+    }
+
+    internal class SetDocumentState : State
+    {
+        private DeviceType dev;
+        private uint money;
+
+        public SetDocumentState(uint money, DeviceType dev)
+        {
+            this.money = money;
+            this.dev = dev;
+        }
+
+        internal override State CheckOut(out uint money)
+        {
+            money = this.money;
+            return new EmptyState();
+        }
+
+        internal override State ChooseDevice(DeviceType dev)
+        {
+            throw new Exception();
+        }
+
+        internal override State InsertMoney(uint money)
+        {
+            throw new Exception();
+        }
+
+        internal override State Print(out string result)
+        {
+            throw new Exception();
+        }
+
+        internal override State SetDocument(Document doc)
+        {
+            return new PrintState(money, dev, doc);
+        }
+    }
+
+    internal class PrintState : State
+    {
+        private DeviceType dev;
+        private Document doc;
+        private uint money;
+
+        public PrintState(uint money, DeviceType dev, Document doc)
+        {
+            this.money = money;
+            this.dev = dev;
+            this.doc = doc;
+        }
+
+        internal override State CheckOut(out uint money)
+        {
+            throw new Exception();
+        }
+
+        internal override State ChooseDevice(DeviceType dev)
+        {
+            throw new Exception();
+        }
+
+        internal override State InsertMoney(uint money)
+        {
+            throw new Exception();
+        }
+
+        internal override State Print(out string result)
+        {
+            result = doc.Data;
+            money--;
+            return new SetDocumentState(money, dev);
+        }
+
+        internal override State SetDocument(Document doc)
+        {
+            throw new Exception();
+        }
+    }
+}
